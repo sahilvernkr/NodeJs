fba9a8cde41aa896a176492adec09b9c23e15896
sahil <sahilvernekar96@gmail.com>
2024-10-22 22:12:50 +0530

    create simple API

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = [
+    { name: 'sahil vernekar', email: 'sahil@test.com' },
+    { name: 'sam', email: 'sam@test.com' },
+    { name: 'peter', email: 'peter@test.com' }
+];
+module.exports = data;
--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-
-console.log("nodemon fixed");
+const http = require('http');
+const data = require('./data');
+http.createServer((req, resp) => {
+    resp.writeHead(200, { 'content-type': 'application\json' });
+    resp.write(JSON.stringify(data));
+    resp.end();
+}).listen(5000);
