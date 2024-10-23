cbc2703ff68c865fa97892dd1b1bab0b1ae7670c
sahil <sahilvernekar96@gmail.com>
2024-10-23 08:29:37 +0530

    input from command

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const http = require('http');
-const data = require('./data');
-http.createServer((req, resp) => {
-    resp.writeHead(200, { 'content-type': 'application\json' });
-    resp.write(JSON.stringify(data));
-    resp.end();
-}).listen(5000);
+const fs = require('fs');
+
+const input = process.argv;
+
+if (input[2] == 'add') {
+    fs.writeFileSync(input[3], input[4])
+}
+else if (input[2] == 'remove') {
+    fs.unlinkSync(input[3])
+}
+else {
+    console.log('invalid output')
+}
+//run -- node .\index.js add orange.txt 'this is a fruit'
+//run -- node .\index.js remove orange.txt 
